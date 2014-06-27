7a5c238c92fbd8ce284cf06c6a1b1033ecdfcc63
Semen Danilov <samvimes@yandex.ru>
2014-06-27 17:05:04 +0400

    1

--- Game/src/game/controller/KeyboardController.java
+++ Game/src/game/controller/KeyboardController.java
@@ -1,7 +1,9 @@
-package game;
+package game.controller;
+
+import game.controller.model.Controller;
 
 /**
  * Created by Max on 6/27/2014.
  */
-public class KeyboardController {
+public class KeyboardController implements Controller {
 }
--- Game/src/game/controller/NetworkController.java
+++ Game/src/game/controller/NetworkController.java
@@ -1,7 +1,9 @@
 package game.controller;
 
+import game.controller.model.Controller;
+
 /**
  * Created by Max on 6/27/2014.
  */
-public class NetworkController {
+public class NetworkController implements Controller {
 }
