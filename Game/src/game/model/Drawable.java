0702f780ab956e22cbc2fd0aee05416745a1b57c
Semen Danilov <samvimes@yandex.ru>
2014-06-27 14:06:26 +0400

    Changed Drawable

--- Game/src/game/model/Drawable.java
+++ Game/src/game/model/Drawable.java
@@ -1,8 +1,14 @@
 package game.model;
 
+import java.awt.*;
+
 /**
  * Created by Max on 6/27/2014.
  */
 public interface Drawable {
-    public void onRender();
+
+    public void beforeRender();
+
+    public void onRender(final Canvas canvas, final int x, final int y);
+
 }
--- Game/src/game/model/Sprite.java
+++ Game/src/game/model/Sprite.java
@@ -1,7 +1,22 @@
 package game.model;
 
+import java.awt.*;
+
 /**
  * Created by Max on 6/27/2014.
  */
-public class Sprite {
+public class Sprite implements Drawable {
+
+
+
+    @Override
+    public void beforeRender() {
+
+    }
+
+    @Override
+    public void onRender(final Canvas canvas, final int x, final int y) {
+
+    }
+
 }
